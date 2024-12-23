b871367628fe9f4d6b5d905371652ff522b8765d
kirubel-teka <se.kirubel.teka@gmail.com>
2024-12-23 12:22:55 +0300

    mixins

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template> 
+  <div id="app">
+    <div class="scrollPosition"> {{ offset }}</div>
+  </div>
+
+</template>
+
+<script>
+import mixin from '@/mixin';
+export default {
+name: 'App',
+mixins: [mixin],
+}
+
+</script>
+
+
+<style>
+#app {
+width: 100%;
+height: 3000px;
+position: relative;
+}
+.scrollPosition {
+position: fixed;
+left: 10px;
+right: 10px;
+}
+</style>
